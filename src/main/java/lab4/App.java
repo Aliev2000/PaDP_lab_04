14ab197b93b0b11c8b68d702e6e45df99a53abab
Parviz <alievz2011@gmail.com>
2021-12-18 00:48:04 +0300

    Scripted auto-commit on change (2021-12-18 00:48:04) by gitwatch.sh

--- src/main/java/lab4/App.java
+++ src/main/java/lab4/App.java
@@ -23,6 +23,8 @@ import java.util.concurrent.CompletionStage;
 public class App extends AllDirectives {
     public static final int PORT = 8080;
     public static final int TIMEOUT = 5000;
+    private Route createRoute(ActorSystem system) {
+
     private Route post(ActorRef routerActor) {
         return entity(Jackson.unmarshaller(Package.class),
                 msg -> {
