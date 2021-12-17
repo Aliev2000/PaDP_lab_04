d19e108f77d679c0ca5165d273193556335e02cf
Parviz <alievz2011@gmail.com>
2021-12-18 00:44:48 +0300

    Scripted auto-commit on change (2021-12-18 00:44:48) by gitwatch.sh

---
+++ src/main/java/lab4/App.java
@@ -0,0 +1,23 @@
+package lab4;
+
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import scala.concurrent.Future;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+public class App extends AllDirectives {
