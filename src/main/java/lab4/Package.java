e7b5414ccea115aa709e95afbbf1f61fab233b4d
Parviz <alievz2011@gmail.com>
2021-12-18 00:53:26 +0300

    Scripted auto-commit on change (2021-12-18 00:53:26) by gitwatch.sh

--- src/main/java/lab4/Package.java
+++ src/main/java/lab4/Package.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
+    @JsonProperty("packageId")
     public String getJsScript() {
     }
     public List<Test> getTests() {
